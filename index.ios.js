0d8124e91442c08087bba3f0037e1322683f1a67
krittiyaclark <krittiyaclark@gmail.com>
2017-10-24 17:26:55 -0400

    Added and styled header

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,12 @@
 // Import a library to help create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
 // Create a Component
 const App = () => {
   return (
-    <Text>Hello World</Text>
+    <Header headerText={ 'Doctor Listing' } />
   );
 };
 
